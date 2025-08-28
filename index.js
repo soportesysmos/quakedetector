4d315fc2c43ec9db810979d9cf496d3a84f78afa
soportesysmos <soporte.sysmos@gmail.com>
2025-08-27 20:06:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // importante para recibir JSON
 
-// Endpoint que recibe alertas del ESP8266
 app.post('/alert', (req, res) => {
-  console.log('⚠️ Alerta recibida del ESP8266: ', req.body);
+  console.log('⚠️ Alerta recibida del ESP8266:', req.body);
   res.send({ status: 'alert received' });
-  console.log('📌 Request headers:', req.headers); // ayuda a depurar
 });
 
-// Endpoint de prueba
 app.get('/', (req, res) => res.send('Servidor activo!'));
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
